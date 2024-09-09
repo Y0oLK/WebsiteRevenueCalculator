9dc7eee22e37a4a96867983dbf501d967f0640e0
Y0oLK <93226344+Y0oLK@users.noreply.github.com>
2024-09-09 11:10:37 +0100

    Update google_sheets_auth.py

--- utils/google_sheets_auth.py
+++ utils/google_sheets_auth.py
@@ -2,11 +2,9 @@ from google.oauth2.service_account import Credentials
 from gspread import authorize
 
 def authorize_google_sheets():
-    """
-    Authorizes the Google Sheets API using service account credentials.
-    """
+  
     try:
-        # Replace 'path/to/credentials.json' with the path to your service account key file
+       
         credentials = Credentials.from_service_account_file(
             'credentials.json',
             scopes=["https://www.googleapis.com/auth/spreadsheets"]
